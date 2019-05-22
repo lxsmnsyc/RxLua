192f853702793428cc928ac18638033e2f52a48f
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-22 09:34:09 +0800

    Create maybe.lua

---
+++ is/observer/maybe.lua
@@ -0,0 +1,8 @@
+local isErrorObserver = require "RxLua.is.observer.error"
+local isFunction = require "RxLua.utils.function"
+
+return function (observer)
+  return isErrorObserver(observer)
+    and isFunction(observer.onComplete)
+    and isFunction(observer.onSuccess)
+end
