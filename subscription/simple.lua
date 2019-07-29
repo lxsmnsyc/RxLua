aff71f13f94558264558bcd010eb8a436ab9aed9
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-07-29 09:26:15 +0800

    Create simple.lua

---
+++ subscription/simple.lua
@@ -0,0 +1,44 @@
+--[[
+    Reactive Extensions for Lua
+	
+    MIT License
+    Copyright (c) 2019 Alexis Munsayac
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+--]]
+local extend = require "RxLua.utils.extend"
+local typeof = require "RxLua.utils.typeof"
+local create = require "RxLua.utils.create"
+
+local try = require "RxLua.utils.try"
+
+local Subscription = require "RxLua.subscription"
+
+local SimpleSubscription = extend(Subscription)
+
+function SimpleSubscription.new()
+  local instance = create(SimpleSubscription)
+  instance.alive = true
+  return instance
+end
+
+function SimpleSubscription:cancel()
+  if (self.alive) then
+    self.alive = false
+  end
+end
+
+return SimpleSubscription
